9697e764a600883f4b99cbf092e6745dcd3e5f8f
domen111 <domen.public@gmail.com>
2015-08-17 03:24:08 +0800

    TIOJ fetcher

---
+++ oj/tioj.py
@@ -0,0 +1,20 @@
+import urllib.request
+import re
+
+class oj:
+	@staticmethod
+	def fetch_user(uname):
+		with urllib.request.urlopen("http://tioj.ck.tp.edu.tw/users/"+uname) as response:
+			html = response.read().decode("utf-8")
+
+		html = html.split("<tbody>",1)[1].split("</tbody>")[0]
+		html = html.replace("<tr>","").replace("</tr>","")
+		html = html.replace("<td>","").replace("</td>","")
+		html = re.sub(r" {2,}","",html)
+		html = re.sub(r"\n{2,}","\n",html)
+
+		for line in html.split("\n"):
+			if line == "": continue
+			pid = int(re.search(r"problems\/(\d+)\/submissions",line).group(1))
+			if line.find("text-success") != -1:
+				yield pid

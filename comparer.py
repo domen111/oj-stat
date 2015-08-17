daf8766d865dc1c2a1be1c85358e5d505ea503d1
domen111 <domen.public@gmail.com>
2015-08-17 18:07:04 +0800

    comparer show result

--- comparer.py
+++ comparer.py
@@ -1,9 +1,22 @@
 import sys
 from oj import oj
 
+def _fetch(judge_user):
+	judge,user = judge_user.split(":",1)
+	return oj.fetch_user(judge,user)
+
+def compare(expression): #todo
+	return _fetch("tioj:visitorIKC") - _fetch("tioj:domen111") #debug
+
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		print("Error: miss expression")
 		print("See README.md for help")
 		sys.exit()
 	expression = sys.argv[1]
+	result = list(compare(expression))
+	result.sort()
+	print("%d problems in total\n"%len(result))
+	for prob in result:
+		print(prob[0] + " " + str(prob[1]), end="\t")
+	print()

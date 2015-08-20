19c7f22b4daf6091d12944a6fc5e82e18e5f924c
domen111 <domen.public@gmail.com>
2015-08-21 00:21:10 +0800

    Solve the problem of CF problem sorting

--- oj/cf.py
+++ oj/cf.py
@@ -6,6 +6,13 @@ def fetch_user(uname):
 	data = json.loads(html)
 	for i in data["result"]:
 		if i["verdict"] == "OK" :
-			yield str(i["problem"]["contestId"]) + str(i["problem"]["index"])
+			yield cf_prob((i["problem"]["contestId"], i["problem"]["index"]))
+
+class cf_prob(tuple):
+	def __repr__(self):
+		if self[0] >= 100000:
+			return "gym"+str(self[0])+self[1]
+		else:
+			return str(self[0])+self[1]
 	
 # by visitorIKC 15.08.18

1245252badb11732c4cc9cec9274705cdc597d7a
domen111 <domen.public@gmail.com>
2015-08-24 21:22:58 +0800

    Add board.py

---
+++ core/board.py
@@ -0,0 +1,12 @@
+from core.oj import oj
+
+# users_probs data structure:
+#   tuple(users, probs)
+#   users:
+#     dict(user1_name: {oj1: oj1_uid, ...}, ...)
+#   probs:
+#     [prob1_namestr, prob2_namestr, ...]
+# return data structure:
+#   dict(user1_name: [prob1_status_str, prob2_status_str, ...], ...)
+def fetch(users_probs):
+	pass
